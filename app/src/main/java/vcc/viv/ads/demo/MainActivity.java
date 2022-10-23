e88c61c31e6fecd72cb2764e45f1c44482059f51
Duong <duongnguyenthac@vccorp.vn>
2022-10-24 01:33:01 +0700

    first commit

--- app/src/main/java/vcc/viv/ads/demo/MainActivity.java
+++ app/src/main/java/vcc/viv/ads/demo/MainActivity.java
@@ -1,7 +1,5 @@
 package vcc.viv.ads.demo;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.widget.ArrayAdapter;
@@ -14,28 +12,24 @@ import androidx.fragment.app.FragmentTransaction;
 import java.util.ArrayList;
 import java.util.List;
 
-import vcc.viv.ads.demo.bin.BaseActivity;
+import vcc.viv.ads.bin.WebViewType;
 import vcc.viv.ads.demo.bin.Toolkit;
 import vcc.viv.ads.demo.databinding.ActivityMainBinding;
-import vcc.viv.ads.demo.detail.DetailAdsCatFishFragment;
 import vcc.viv.ads.demo.detail.DetailAdsInPageFragment;
 import vcc.viv.ads.demo.detail.DetailAdsNativeFragment;
 import vcc.viv.ads.demo.detail.DetailAdsNonInPage;
 import vcc.viv.ads.demo.detail.DetailAdsPopupFragment;
+import vcc.viv.ads.demo.detail.DetailAdsTrueViewFragment;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
-
-    public static void start(Context context) {
-        Intent starter = new Intent(context, MainActivity.class);
-        starter.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        context.startActivity(starter);
-    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Toolkit toolkit = new Toolkit();
+        toolkit.initAdsSdk(this);
 
         LayoutInflater inflater = LayoutInflater.from(this);
         binding = ActivityMainBinding.inflate(inflater);
@@ -43,15 +37,11 @@ public class MainActivity extends BaseActivity {
 
         List<String> item = new ArrayList<>();
         item.add("Popup");
-        item.add("Popup-Form");
-        item.add("Popup-TMDT");
-        item.add("Popup-Game");
-        item.add("Popup-LiveStream");
-        item.add("Catfish");
         item.add("Inpage");
         item.add("NonInpage");
-        item.add("Native-Home");
-        item.add("Native-Detail");
+        item.add("Home");
+        item.add("Detail");
+        item.add("TrueView");
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, item);
 
@@ -61,32 +51,20 @@ public class MainActivity extends BaseActivity {
                 case "Popup":
                     addFragment(DetailAdsPopupFragment.newInstance());
                     break;
-                case "Popup-Form":
-                    addFragment(DetailAdsPopupFragment.newInstance("form", "https://demo.admicro.vn/tuta/demo/ads-format-in-app-form/index.html"));
-                    break;
-                case "Popup-TMDT":
-                    addFragment(DetailAdsPopupFragment.newInstance("ecommerce", "https://shopee.vn/unilever_vietnam"));
-                    break;
-                case "Popup-Game":
-                    addFragment(DetailAdsPopupFragment.newInstance("game", "https://devminigame.ewings.vn/games/L002/?dev=1"));
-                    break;
-                case "Popup-LiveStream":
-                    addFragment(DetailAdsPopupFragment.newInstance("livestream", "https://demo.admicro.vn/mobile/billboard/ban_270921.html"));
-                    break;
-                case "Catfish":
-                    addFragment(DetailAdsCatFishFragment.newInstance());
-                    break;
-                case "Native-Home":
-                    addFragment(DetailAdsNativeFragment.newInstance(1));
-                    break;
-                case "Native-Detail":
-                    addFragment(DetailAdsNativeFragment.newInstance(0));
+                case "TrueView":
+                    addFragment(DetailAdsTrueViewFragment.newInstance(WebViewType.playerLive.tag));
                     break;
                 case "Inpage":
                     addFragment(DetailAdsInPageFragment.newInstance());
                     break;
                 case "NonInpage":
                     addFragment(DetailAdsNonInPage.newInstance());
+                    break;
+                case "Home":
+                    addFragment(DetailAdsNativeFragment.newInstance(R.string.native_home));
+                    break;
+                case "Detail":
+                    addFragment(DetailAdsNativeFragment.newInstance(R.string.native_detail));
                     break;
                 default:
             }
